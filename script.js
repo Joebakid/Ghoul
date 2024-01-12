b73fa44667ef47baa8208e92a7c8a91eeebe04d3
joebakid <josephbawo@gmail.com>
2024-01-12 22:37:54 +0100

    correction

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 gsap.fromTo(
   "nav",
-  { opacity: 0, yPercent: -100 },
-  { opacity: 1, yPercent: 0 },
+  { opacity: 0, yPercent: -100, scale: 0.22 },
+  { opacity: 1, yPercent: 0, scale: 1 },
   ">"
 );
 gsap.fromTo(
   "header",
-  { opacity: 0, yPercent: 100 },
-  { opacity: 1, yPercent: 0, duration: 1 },
+  { opacity: 0, yPercent: 100, scale: 0.22 },
+  { opacity: 1, yPercent: 0, duration: 1, scale: 1 },
   "<50%"
 );
 // gsap.fromTo("body", { opacity: 0 }, { opacity: 1 }, ">");
