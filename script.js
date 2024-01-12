9fe9f1f85b6d9f7c6eb423323e3cf5a16e2b3688
joebakid <josephbawo@gmail.com>
2024-01-12 20:41:55 +0100

    page done

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 //  preloader
 
 gsap.fromTo(
-  ".preloader-logo",
-  { scale: 0.1, opacity: 1 },
-  { scale: 1, duration: 5, opacity: 0 },
-  ">"
-);
-
-gsap.fromTo(
-  ".overlay",
-  { autoAlpha: 1 },
-  { autoAlpha: 0, backgroundColor: "transparent" },
-  ">"
-);
-
-gsap.fromTo(
   "nav",
   { opacity: 0, yPercent: -100 },
-  { opacity: 1, yPercent: 0, duration: 1 },
+  { opacity: 1, yPercent: 0 },
   ">"
 );
 gsap.fromTo(
   "header",
   { opacity: 0, yPercent: 100 },
-  { opacity: 1, yPercent: 0, duration: 2 },
+  { opacity: 1, yPercent: 0, duration: 1 },
   "<50%"
 );
 // gsap.fromTo("body", { opacity: 0 }, { opacity: 1 }, ">");
