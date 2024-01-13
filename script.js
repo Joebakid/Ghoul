2554a17a9906e65fd8de259744a60048d06af534
joebakid <josephbawo@gmail.com>
2024-01-13 11:08:34 +0100

    preloader added

--- script.js
+++ script.js
@@ -2,14 +2,23 @@
 
 gsap.fromTo(
   "nav",
-  { opacity: 0, yPercent: -100, scale: 0.22 },
-  { opacity: 1, yPercent: 0, scale: 1 },
+  { opacity: 0, yPercent: -100 },
+  { opacity: 1, yPercent: 0 },
   ">"
 );
 gsap.fromTo(
   "header",
-  { opacity: 0, yPercent: 100, scale: 0.22 },
-  { opacity: 1, yPercent: 0, duration: 1, scale: 1 },
+  { opacity: 0, yPercent: 100 },
+  { opacity: 1, yPercent: 0, duration: 1 },
   "<50%"
 );
 // gsap.fromTo("body", { opacity: 0 }, { opacity: 1 }, ">");
+
+//
+document.addEventListener("DOMContentLoaded", function () {
+  // Simulate content loading delay
+  setTimeout(function () {
+    // Hide the preloader
+    document.body.classList.add("loaded");
+  }, 3000); // Adjust the timeout as needed
+});
