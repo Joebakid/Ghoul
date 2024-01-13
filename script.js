e9673d2dbc7c18ea6666fc5bed0dba7f311aa04b
joebakid <josephbawo@gmail.com>
2024-01-13 20:12:06 +0100

    added email.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.getElementById("form").addEventListener("submit", function (event) {
   btn.value = "Sending...";
 
   const serviceID = "default_service";
-  const templateID = "template_gn0bw38";
+  const templateID = "template_750pn2r";
 
   emailjs.sendForm(serviceID, templateID, this).then(
     () => {
@@ -79,3 +79,24 @@ document.getElementById("form").addEventListener("submit", function (event) {
     }
   );
 });
+
+// const btn = document.getElementById('button');
+
+// document.getElementById('form')
+//  .addEventListener('submit', function(event) {
+//    event.preventDefault();
+
+//    btn.value = 'Sending...';
+
+//    const serviceID = 'default_service';
+//    const templateID = 'template_750pn2r';
+
+//    emailjs.sendForm(serviceID, templateID, this)
+//     .then(() => {
+//       btn.value = 'Send Email';
+//       alert('Sent!');
+//     }, (err) => {
+//       btn.value = 'Send Email';
+//       alert(JSON.stringify(err));
+//     });
+// });
