d52ff50d806082c417d9cc76d3fc84f64b498020
joebakid <josephbawo@gmail.com>
2024-01-13 15:11:36 +0100

    data received

--- script.js
+++ script.js
@@ -24,3 +24,58 @@ document.addEventListener("DOMContentLoaded", function () {
     // Additional GSAP animations go here
   }, 3000); // Adjust the timeout as needed
 });
+
+// FORM
+document.addEventListener("DOMContentLoaded", function () {
+  // Simulate content loading delay
+  setTimeout(function () {
+    // Hide the preloader
+    document.body.classList.add("loaded");
+
+    // Your GSAP animations go here
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+      "nav",
+      { opacity: 0, yPercent: -100 },
+      { opacity: 1, yPercent: 0 },
+      ">"
+    );
+
+    tl.fromTo(
+      "header",
+      { opacity: 0, yPercent: 100 },
+      { opacity: 1, yPercent: 0, duration: 1 },
+      "<50%"
+    );
+
+    // Additional GSAP animations go here
+  }, 3000); // Adjust the timeout as needed
+});
+
+// FORM
+
+const btn = document.getElementById("button");
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "default_service";
+  const templateID = "template_gn0bw38";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+
+      // Clear the form input fields
+      this.reset();
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
