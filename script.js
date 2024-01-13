f9d553711c2de64d0894a3df7a60670076bc00ac
joebakid <josephbawo@gmail.com>
2024-01-13 11:11:05 +0100

    preloader integration with gsap

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-//  preloader
-
-gsap.fromTo(
-  "nav",
-  { opacity: 0, yPercent: -100 },
-  { opacity: 1, yPercent: 0 },
-  ">"
-);
-gsap.fromTo(
-  "header",
-  { opacity: 0, yPercent: 100 },
-  { opacity: 1, yPercent: 0, duration: 1 },
-  "<50%"
-);
-// gsap.fromTo("body", { opacity: 0 }, { opacity: 1 }, ">");
-
-//
 document.addEventListener("DOMContentLoaded", function () {
   // Simulate content loading delay
   setTimeout(function () {
     // Hide the preloader
     document.body.classList.add("loaded");
+
+    // Your GSAP animations go here
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+      "nav",
+      { opacity: 0, yPercent: -100 },
+      { opacity: 1, yPercent: 0 },
+      ">"
+    );
+
+    tl.fromTo(
+      "header",
+      { opacity: 0, yPercent: 100 },
+      { opacity: 1, yPercent: 0, duration: 1 },
+      "<50%"
+    );
+
+    // Additional GSAP animations go here
   }, 3000); // Adjust the timeout as needed
 });
